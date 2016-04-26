c4a39ec08a4551f31809b9531ade334d10fd957c
thomasweld <thomas.weld@gmail.com>
2016-04-26 15:01:13 -0400

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+import token from './githubtoken';
+
+$.ajaxSetup({
+  headers: {
+    Authorization: "token " + token
+  }
+});
+
+var url = 'https://api.github.com/users/thomasweld';
+
+$.getJSON(url, function ( res ){
+  console.log(res);
+});
